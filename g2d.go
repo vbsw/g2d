c4b2c2cfa3ac41aa361c6b466c5d9b1669ef5e1e
Vitali Baumtrok <vbsw@mailbox.org>
2023-01-25 00:20:50 +0100

    rectangle layer

--- g2d.go
+++ g2d.go
@@ -17,6 +17,10 @@ import (
 )
 
 const (
+	wrongLayerType = "cast to wrong layer type"
+)
+
+const (
 	configType = iota
 	createType
 	showType
@@ -137,6 +141,16 @@ func (gfx *Graphics) SetVSync(vsync bool) {
 	}
 }
 
+func (gfx *Graphics) NewRectLayer(size int) int {
+	layerId := len(gfx.wPool.layers)
+	gfx.wPool.layers = append(gfx.wPool.layers, newLayerRects(layerId, size))
+	return layerId
+}
+
+func (gfx *Graphics) ReleaseRect(rect *Rect) {
+	gfx.wPool.layers[rect.layer].release(rect.chunk, rect.index)
+}
+
 func (gfx *Graphics) updateRPool() {
 	gfx.mutex.Lock()
 	indexCurr := gfx.state * 4
@@ -157,6 +171,12 @@ func (gfx *Graphics) switchWPool() {
 	gfx.mutex.Unlock()
 }
 
+type Rect struct {
+	layer, chunk, index int
+	X, Y, W, H          int
+	R, G, B, A          float32
+}
+
 type tErrorGenerator interface {
 	ToError(g2dErrNum, win32ErrNum uint64, info string) error
 }
@@ -170,10 +190,17 @@ type tErrorHandler struct {
 
 type tPool struct {
 	bgR, bgG, bgB C.float
+	layers        []tLayer
 }
 
 func (pool *tPool) set(other *tPool) {
 	pool.bgR, pool.bgG, pool.bgB = other.bgR, other.bgG, other.bgB
+	for i, layer := range pool.layers {
+		layer.set(other.layers[i])
+	}
+	for _, otherLayer := range pool.layers[len(pool.layers):] {
+		pool.layers = append(pool.layers, otherLayer.clone())
+	}
 }
 
 type tMainLoop struct {
@@ -271,6 +298,135 @@ type tWindowError struct {
 }
 
 type tStopMainLoop struct {
+}
+
+type tLayer interface {
+	newRect() *Rect
+	set(other tLayer)
+	release(chunk, index int)
+	clone() tLayer
+}
+
+type tLayerBase struct {
+	id, size    int
+	totalActive int
+	active      [][]bool
+	usage       [][]int
+}
+
+func (layer *tLayerBase) initBase(id, size int) {
+	layer.id = id
+	layer.size = size
+	layer.active = make([][]bool, 1)
+	layer.usage = make([][]int, 1)
+	layer.active[0] = make([]bool, size)
+	layer.usage[0] = make([]int, 1, size+1)
+}
+
+func (layer *tLayerBase) nextIndex() (int, int) {
+	layer.totalActive++
+	for chunk, usage := range layer.usage {
+		if len(usage) == 1 {
+			nextIndex := usage[0]
+			if nextIndex+1 < cap(usage) {
+				usage[0]++
+				layer.active[chunk][nextIndex] = true
+				return chunk, nextIndex
+			}
+		} else {
+			newLength := len(usage) - 1
+			nextIndex := usage[newLength]
+			usage[0]++
+			layer.usage[chunk] = usage[:newLength]
+			return chunk, nextIndex
+		}
+	}
+	layer.size *= 2
+	layer.usage = append(layer.usage, make([]int, 1, layer.size+1))
+	return len(layer.usage) - 1, 0
+}
+
+func (layer *tLayerBase) release(chunk, index int) {
+	layer.totalActive--
+	layer.active[chunk][index] = false
+	layer.usage[chunk][0]--
+	layer.usage[chunk] = append(layer.usage[chunk], index)
+}
+
+func (layer *tLayerBase) cloneBase(other *tLayerBase) {
+	layer.id = other.id
+	layer.size = other.size
+	layer.totalActive = other.totalActive
+	layer.active = make([][]bool, len(other.active), cap(other.active))
+	layer.usage = make([][]int, len(other.usage), cap(other.usage))
+	for i, otherActive := range other.active {
+		layer.active[i] = make([]bool, len(otherActive), cap(otherActive))
+		copy(layer.active[i], otherActive)
+	}
+	for i, otherUsage := range other.usage {
+		layer.usage[i] = make([]int, len(otherUsage), cap(otherUsage))
+		copy(layer.usage[i], otherUsage)
+	}
+}
+
+func (layer *tLayerBase) setBase(other *tLayerBase) {
+	layer.size = other.size
+	layer.totalActive = other.totalActive
+	for i, otherActive := range other.active {
+		layer.active[i] = layer.active[i][:len(otherActive)]
+		copy(layer.active[i], otherActive)
+	}
+	for i, otherUsage := range other.usage {
+		layer.usage[i] = layer.usage[i][:len(otherUsage)]
+		copy(layer.usage[i], otherUsage)
+	}
+}
+
+type tLayerRects struct {
+	tLayerBase
+	rects [][]Rect
+}
+
+func (layer *tLayerRects) newRect() *Rect {
+	chunk, index := layer.nextIndex()
+	if chunk == len(layer.rects) {
+		layer.rects = append(layer.rects, make([]Rect, layer.size))
+	}
+	rect := &layer.rects[chunk][index]
+	rect.layer, rect.chunk, rect.index = layer.id, chunk, index
+	return rect
+}
+
+func (layer *tLayerRects) clone() tLayer {
+	other := new(tLayerRects)
+	other.cloneBase(&layer.tLayerBase)
+	other.rects = make([][]Rect, len(layer.rects), cap(layer.rects))
+	for i, rects := range layer.rects {
+		other.rects[i] = make([]Rect, len(rects), cap(rects))
+		copy(other.rects[i], rects)
+	}
+	return other
+}
+
+func (layer *tLayerRects) set(other tLayer) {
+	otherLayer, ok := other.(*tLayerRects)
+	if ok {
+		layer.setBase(&otherLayer.tLayerBase)
+		for i, otherRects := range otherLayer.rects {
+			layer.rects[i] = layer.rects[i][:len(otherRects)]
+			copy(layer.rects[i], otherRects)
+		}
+	} else {
+		panic(wrongLayerType)
+	}
+}
+
+func newLayerRects(id, size int) *tLayerRects {
+	layer := new(tLayerRects)
+	layer.rects = make([][]Rect, 1)
+	layer.rects[0] = make([]Rect, size)
+	layer.initBase(id, size)
+	return layer
 }
 
 func newConfiguration() *Configuration {
--- g2d_windows_test.go
+++ g2d_windows_test.go
@@ -18,9 +18,30 @@ type tTestConfigWindow struct {
 	destroyCalled bool
 }
 
+func (window *tTestConfigWindow) OnConfig(config *Configuration) error {
+	config.AutoUpdate = false
+	window.configCalled = true
+	return errors.New("test")
+}
+
+func (window *tTestConfigWindow) OnDestroy() {
+	window.destroyCalled = true
+}
+
 type tTestCreateWindow struct {
 	tTestConfigWindow
 	createCalled bool
+}
+
+func (window *tTestCreateWindow) OnConfig(config *Configuration) error {
+	config.AutoUpdate = false
+	window.configCalled = true
+	return nil
+}
+
+func (window *tTestCreateWindow) OnCreate(widget *Widget) error {
+	window.createCalled = true
+	return errors.New("test")
 }
 
 func TestInit(t *testing.T) {
@@ -81,23 +102,41 @@ func TestCreateWindow(t *testing.T) {
 	}
 }
 
-func (window *tTestConfigWindow) OnConfig(config *Configuration) error {
-	config.AutoUpdate = false
-	window.configCalled = true
-	return errors.New("test")
+func TestLayerA(t *testing.T) {
+	layer := newLayerRects(0, 2)
+	rectA := layer.newRect()
+	rectB := layer.newRect()
+	if rectA.layer != 0 || rectB.layer != 0 {
+		t.Error(rectA.layer, rectB.layer)
+	} else if rectA.chunk != 0 || rectB.chunk != 0 {
+		t.Error(rectA.chunk, rectB.chunk)
+	} else if rectA.index != 0 || rectB.index != 1 {
+		t.Error(rectA.index, rectB.index)
+	} else {
+		layer.release(rectA.chunk, rectA.index)
+		rectA = layer.newRect()
+		rectB = layer.newRect()
+		if layer.totalActive != 3 || layer.size != 4 {
+			t.Error(layer.totalActive, layer.size)
+		} else if rectA.layer != 0 || rectB.layer != 0 {
+			t.Error(rectA.layer, rectB.layer)
+		} else if rectA.chunk != 0 || rectB.chunk != 1 {
+			t.Error(rectA.chunk, rectB.chunk)
+		} else if rectA.index != 0 || rectB.index != 0 {
+			t.Error(rectA.index, rectB.index)
+		}
+	}
 }
 
-func (window *tTestConfigWindow) OnDestroy() {
-	window.destroyCalled = true
-}
-
-func (window *tTestCreateWindow) OnConfig(config *Configuration) error {
-	config.AutoUpdate = false
-	window.configCalled = true
-	return nil
-}
-
-func (window *tTestCreateWindow) OnCreate(widget *Widget) error {
-	window.createCalled = true
-	return errors.New("test")
+func TestLayerB(t *testing.T) {
+	layer := newLayerRects(0, 2)
+	layer.newRect()
+	rectB := layer.newRect()
+	layer.newRect()
+	rectD := *rectB
+	layer.release(rectB.chunk, rectB.index)
+	rectB = rectB
+	if rectD != *rectB {
+		t.Error(rectD.layer, rectD.chunk, rectD.index, rectB.layer, rectB.chunk, rectB.index)
+	}
 }
